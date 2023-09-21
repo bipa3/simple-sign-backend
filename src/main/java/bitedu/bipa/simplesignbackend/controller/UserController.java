b01eddb7fffdc1701279b27ccc2cdeb16bf81f79
DESKTOP-EPB3CDE\KKM <kmi_kim@naver.com>
2023-09-21 17:47:46 +0900

    feat : 로그인 기능 구현

---
+++ src/main/java/bitedu/bipa/simplesignbackend/controller/UserController.java
@@ -0,0 +1,37 @@
+package bitedu.bipa.simplesignbackend.controller;
+
+import bitedu.bipa.simplesignbackend.model.dto.UserDTO;
+import bitedu.bipa.simplesignbackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity userLogin(@RequestBody UserDTO userDTO, HttpSession session){
+        UserDTO userDTO2 = userService.loginUser(userDTO.getLoginId(),userDTO.getPassword());
+        if(userDTO2 != null){
+
+            String userId = String.valueOf(userDTO2.getUserId());
+            String userName = userDTO2.getUserName();
+            session.setAttribute("userId", userId);
+            session.setAttribute("userName", userName);
+
+            String sessionUserId = (String) session.getAttribute("userId");
+            String sessionUserName = (String) session.getAttribute("userName");
+            System.out.println("Session UserId : " + sessionUserId + "Session UserName : " + sessionUserName);
+
+            return new ResponseEntity(HttpStatus.OK);
+        } else {
+          return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+}
