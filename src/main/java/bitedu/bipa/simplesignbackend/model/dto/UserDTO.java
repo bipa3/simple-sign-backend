5dec4e96acb18109ff14d5753783bbcdd52a15d6
DESKTOP-EPB3CDE\KKM <kmi_kim@naver.com>
2023-10-12 17:36:00 +0900

    feat : 개인정보 조회 및 수정 (프로필, 서명 제외)

--- src/main/java/bitedu/bipa/simplesignbackend/model/dto/UserDTO.java
+++ src/main/java/bitedu/bipa/simplesignbackend/model/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package bitedu.bipa.simplesignbackend.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,8 +17,10 @@ public class UserDTO {
     private String address;
     private String password;
     private String email;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date startDate;
     private char gender;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date birth;
     private boolean employmentStatus;
     private String loginId;
@@ -25,4 +28,11 @@ public class UserDTO {
 
     // 비밀번호 암호화
     private String salt;
+
+    //개인정보
+    private String compName;
+    private String estName;
+    private String deptString;
+    private String positionName;
+    private String gradeName;
 }
