d773a21e97b0d0c5e125066649955d1e21fc8152
jmlbw <144968527+jmlbw@users.noreply.github.com>
2023-10-12 18:34:35 +0900

    tset1

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ echo "Gradle 빌드를 시작합니다..."
 $GRADLE $BUILD_COMMAND
 
 if [ $? -eq 0 ]; then
-  echo "Gradle 빌드가 성공적으로 완료되었습니다."
+  echo "Gradle 빌드가 성공적으로 완료되었습니다." 
 else
   echo "Gradle 빌드에 실패했습니다. 빌드 스크립트를 확인하세요."
   exit 1
