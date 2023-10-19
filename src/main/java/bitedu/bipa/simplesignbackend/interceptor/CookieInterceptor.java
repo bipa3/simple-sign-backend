8eb30e6b05ea8c759feb62cf9487e1e0d8b9d45f
kyungmi <kkm042689@gmail.com>
2023-10-19 20:43:03 +0900

    fix: 쿠키 인터셉터 수정

--- src/main/java/bitedu/bipa/simplesignbackend/interceptor/CookieInterceptor.java
+++ src/main/java/bitedu/bipa/simplesignbackend/interceptor/CookieInterceptor.java
@@ -1,6 +1,6 @@
     package bitedu.bipa.simplesignbackend.interceptor;
-;
-    import org.springframework.stereotype.Component;
+
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -13,14 +13,14 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
         @Override
         public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-            //response.setHeader("Set-Cookie", "JSESSIONID=" + request.getRequestedSessionId() + "; path=/; Secure; SameSite=None");
+            //response.setHeader("Set-Cookie", "JSESSIONID=" + RequestContextHolder.getRequestAttributes().getSessionId() + "; path=/; Secure; SameSite=None");
+            //response.setHeader("Set-Cookie", "JSESSIONID=" + RequestContextHolder.getRequestAttributes().getSessionId() + "; path=/; Secure; SameSite=None");
 
             return true;
         }
 
         @Override
         public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-            System.out.println(RequestContextHolder.getRequestAttributes().getSessionId());
-            response.setHeader("Set-Cookie", "JSESSIONID=" + RequestContextHolder.getRequestAttributes().getSessionId() + "; path=/; Secure; SameSite=None");
+//            response.setHeader("Set-Cookie", "JSESSIONID=" + RequestContextHolder.currentRequestAttributes().getSessionId() + "; path=/; Secure; SameSite=None");
         }
     }
