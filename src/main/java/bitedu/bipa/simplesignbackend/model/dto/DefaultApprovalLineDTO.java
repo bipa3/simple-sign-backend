0e4c8e7315de9d7167abe7165f07d7c734f16864
jmlbw <144968527+jmlbw@users.noreply.github.com>
2023-10-27 11:30:35 +0900

    Feat: 기본결재라인

--- src/main/java/bitedu/bipa/simplesignbackend/model/dto/DefaultApprovalLineDTO.java
+++ src/main/java/bitedu/bipa/simplesignbackend/model/dto/DefaultApprovalLineDTO.java
@@ -6,11 +6,15 @@ import lombok.Data;
 public class DefaultApprovalLineDTO {
     private int compId;
     private String compName;
-    private int est_id;
-    private String est_name;
+    private int estId;
+    private String estName;
     private int deptId;
     private String deptName;
     private int userId;
     private String userName;
     private int lineOrder;
+    private int gradeCode;
+    private int positionCode;
+    private String gradeName;
+    private String positionName;
 }
