37c7d9a3e2e100ec007eac09b735840c1c8c7db4
DESKTOP-EPB3CDE\KKM <kmi_kim@naver.com>
2023-11-09 21:15:12 +0900

    feat: 알림 기능 구현

--- src/main/java/bitedu/bipa/simplesignbackend/dao/AlarmDAO.java
+++ src/main/java/bitedu/bipa/simplesignbackend/dao/AlarmDAO.java
@@ -1,8 +1,12 @@
 package bitedu.bipa.simplesignbackend.dao;
 
 import bitedu.bipa.simplesignbackend.mapper.AlarmMapper;
+import bitedu.bipa.simplesignbackend.model.dto.AlarmDTO;
 import bitedu.bipa.simplesignbackend.model.dto.AlarmReqDTO;
+import bitedu.bipa.simplesignbackend.model.dto.UserApprovalDTO;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class AlarmDAO {
@@ -19,6 +23,30 @@ public class AlarmDAO {
 
     public String selectDefaultMessage(String alarmCode) {
         return alarmMapper.selectDefaultMessage(alarmCode);
+    }
+
+    public List<AlarmDTO> selectAlarm(int orgUserId){
+        return alarmMapper.alarmSelect(orgUserId);
+    }
+
+    // 문서명 들고오기
+    public UserApprovalDTO selectApprovalDocTitle(String alarmDate, int orgUserId, String alarmCode, int approvalDocId){
+        return alarmMapper.selectApprovalDocTitle(alarmDate, orgUserId, alarmCode, approvalDocId);
+    }
+
+    // 결재자 들고오기
+    public UserApprovalDTO selectApprovalUserName(int alarmId){
+        return alarmMapper.selectApprovalUserName(alarmId);
+    }
+
+    // 안읽은 알림 총 갯수
+    public int alarmCount(int orgUserId){
+        return alarmMapper.AlarmCount(orgUserId);
+    }
+
+    // 알림 읽음 처리
+    public boolean updateConfirmationStatus(int alarmId){
+        return alarmMapper.updateConfirmationStatus(alarmId);
     }
 
 }
