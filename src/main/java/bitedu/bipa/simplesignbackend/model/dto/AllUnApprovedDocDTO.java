ab70bc15c3de3e958f09b2e8fb24bb51e17fe99d
brightvvater <hscho199463@gmail.com>
2023-11-14 09:18:27 +0900

    feat: 일괄결재 수정

---
+++ src/main/java/bitedu/bipa/simplesignbackend/model/dto/AllUnApprovedDocDTO.java
@@ -0,0 +1,13 @@
+package bitedu.bipa.simplesignbackend.model.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class AllUnApprovedDocDTO {
+
+    private int approvalDocId;
+    private int version;
+
+}
