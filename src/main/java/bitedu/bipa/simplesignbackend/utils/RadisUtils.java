ebabb2ef1a433a61202e91d50e7f08b31fa6d6e5
brightvvater <hscho199463@gmail.com>
2023-11-15 16:35:27 +0900

    feat: redis수정

--- src/main/java/bitedu/bipa/simplesignbackend/utils/RadisUtils.java
+++ src/main/java/bitedu/bipa/simplesignbackend/utils/RadisUtils.java
@@ -7,14 +7,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class RadisUtils {
-    @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
-
-    public void saveData(String key, Object value) {
-        redisTemplate.opsForValue().set(key, value);
-    }
-
-    public Object getData(String key) {
-        return redisTemplate.opsForValue().get(key);
-    }
+//    @Autowired
+//    private RedisTemplate<String, Object> redisTemplate;
+//
+//    public void saveData(String key, Object value) {
+//        redisTemplate.opsForValue().set(key, value);
+//    }
+//
+//    public Object getData(String key) {
+//        return redisTemplate.opsForValue().get(key);
+//    }
 }
